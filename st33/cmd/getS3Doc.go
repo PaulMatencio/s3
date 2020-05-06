2bdb4146cc4767104c92c5bee77254cd51726e9a
Paul Matencio <paulo.matencio@gmail.com>
2020-05-06 22:53:58 +0200

    add statS3Doc

--- st33/cmd/getS3Doc.go
+++ st33/cmd/getS3Doc.go
@@ -293,13 +293,13 @@ func getDocP(key string ) (int) {
 func statDocP(key string ) (int) {
 	var (
 		n = 0
-		key = strings.Replace(key," ","_",-1)
-		buck = bucket +"-"+fmt.Sprintf("%02d",utils.HashKey(key,bucketNumber))
+		key1 = strings.Replace(key," ","_",-1)
+		buck = bucket +"-"+fmt.Sprintf("%02d",utils.HashKey(key1,bucketNumber))
 	)
 	if reverse {
-		key= utils.Reverse(key)
+		key1= utils.Reverse(key1)
 	}
-	KEYx := key+".1"
+	KEYx := key1+".1"
 	svc := s3.New(api.CreateSession())
 	req := datatype.StatObjRequest{
 		Service : svc,
