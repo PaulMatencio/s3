0449456b9fcaed8b1fee5d030163bb682add752d
Paul Matencio <paulo.matencio@gmail.com>
2020-11-15 12:56:18 +0100

    add MB/sec

--- sc/cmd/getMultipart.go
+++ sc/cmd/getMultipart.go
@@ -90,7 +90,9 @@ func getMultipart(cmd *cobra.Command, args []string) {
 	}
 
 	if n, err := api.GetMultipart(req); err == nil {
-		gLog.Info.Printf("Downloaded %s to folder %s - size %d - Elapsed time %v ",key,odir, n,time.Since(start))
+		elapsed := time.Since(start)
+		mbs:= n/(1024*1024* int64(elapsed))
+		gLog.Info.Printf("Downloaded %s to folder: %s - size: %d - MB/sec: %d - Elapsed time: %v ",key,odir, n,mbs,elapsed)
 	} else {
 		gLog.Error.Printf("%v", err)
 	}
