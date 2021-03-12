02855f099e2571b24db37c6c012f24cbfe6503f3
Paul Matencio <paulo.matencio@gmail.com>
2021-03-12 10:32:37 +0100

    Fix listPrefix segmentation violation

--- sindexd/cmd/listPrefix.go
+++ sindexd/cmd/listPrefix.go
@@ -139,7 +139,7 @@ func listPref (prefix string)  {
 		iIndex = "NP"
 	}
 
-	gLog.Info.Printf("Prefix: %s - start with this key: %s - Index: %s - Index specification: %v", prefix,marker,iIndex,*indSpecs[iIndex])
+	gLog.Info.Printf("Prefix: %s - start with this key: %s - Index: %s - Index-ids specification table: %v", prefix,marker,iIndex,&indSpecs)
 	n:= 0
 	for Nextmarker {
 		if response = directory.GetSerialPrefix(iIndex, prefix, delimiter, marker, maxKey, indSpecs); response.Err == nil {
